b76d8208166481ba3a22b8b0de514e0350633534
Steele <stablesc@mail.uc.edu>
2022-09-22 23:56:23 -0400

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        double itemPrice = 100;
+        double taxRate = .05;
+        double salesTax = itemPrice * taxRate;
+
+        System.out.println("For an item that costs $" + itemPrice + ", the sales tax is $" + salesTax + " ." );
     }
 }
